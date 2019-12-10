56f13bf5a403298bd07aa7a700000718a83225d6
amit1881 <amit1881@users.noreply.github.com>
2019-12-11 02:36:03 +0530

    Update DashBoardTest.java

--- src/test/java/org/apps/bhsi/tests/DashBoardTest.java
+++ src/test/java/org/apps/bhsi/tests/DashBoardTest.java
@@ -16,7 +16,7 @@ public class DashBoardTest extends FuncTest{
 	@BeforeClass
 	public void policyLogin() throws Throwable{
 		pl=(PolicyLogin) browser.navigateToUrl(getTestProperty("POLICY_SYSTEM_URL"), "org.apps.bhsi.pages.PolicyLogin");
-		db=pl.login("bhunt", "password");
+		db=pl.login("uname", "pwd");
 	}
 	
 	@Test
